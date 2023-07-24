78b35ebf375133ead019bbf274897d70ea3f10a0
6606021631041 <139095240+phatsakonart@users.noreply.github.com>
2023-07-24 09:59:02 +0700

    Add files via upload

---
+++ exercise 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int Score;
+	cout<<"Enter You pass with score : ";
+	cin>>Score;
+	cout<<Score <<"is"<< (Score>=50? "pass" :"fail");
+	cout<<"Score"<<endl;
+	system("pause");
+	return(0);
+}
